0d88e2ee64d091babb3583a5b66af2e7243646aa
Sportik2020 <114169865+Sportik2020@users.noreply.github.com>
2022-12-04 19:10:46 +0300

    Add files via upload

---
+++ settings.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+#include <SFML/Graphics.hpp>
+
+const float W_WIDTH = 800;
+const float W_HEIGHT = 600;
+const std::string W_TITLE = "lesson 3";
+
+const float BAT_WIDTH = 20.f;
+const float BAT_HEIGHT = 80.f;
+const float BAT_OFFSET = 50.f;
+
+const sf::Color LEFT_BAD_COLOR{ sf::Color::White };
+const sf::Color RIGHT_BAD_COLOR{ sf::Color::White };
+
+const float RADIUS = 15.f;
+const sf::Color CIRCLE_COLOR{ sf::Color::Green };
+
+const float FPS = 60.f;
+
+float speed_x = 2.f;
+float speed_y = 3.f;
+
